28327148eeeae569508090818d0ced8ebbbca6df
suzt-0 <suztbhttr2@gmail.com>
2025-06-23 21:52:18 +0545

    add a seeder for initialization

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,11 +15,37 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
+        User::factory()->createMany([
+            [
             'name' => 'admin',
             'email' => 'test@example.com',
             'password' => bcrypt('admin@123'),
             'role' => 'admin'
+            ],
+            [
+            'name' => 'doc1',
+            'email' => 'doc1@example.com',
+            'password' => bcrypt('doc1@123'),
+            'role' => 'doctor'
+            ],
+            [
+            'name' => 'doc2',
+            'email' => 'doc2@example.com',
+            'password' => bcrypt('doc2@123'),
+            'role' => 'doctor'
+            ],
+            [
+            'name' => 'doc3',
+            'email' => 'doc3@example.com',
+            'password' => bcrypt('doc3@123'),
+            'role' => 'doctor'
+            ],
+            [
+            'name' => 'rep1',
+            'email' => 'rep1@example.com',
+            'password' => bcrypt('rep1@123'),
+            'role' => 'frontdesk'
+            ]
         ]);
     }
 }
