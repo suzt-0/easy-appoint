ff8b927dd090babf4c44f84664de88d2fdf6dc47
suzt-0 <suztbhttr2@gmail.com>
2025-06-23 21:52:02 +0545

    add role to the user

--- app/Models/User.php
+++ app/Models/User.php
@@ -31,7 +31,6 @@ class User extends Authenticatable
      */
     protected $hidden = [
         'password',
-        'role',
         'remember_token',
     ];
 
--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->enum('role',['admin', 'doctor', 'frontDesk' ])->default('frontDesk');
+            $table->enum('role',['admin', 'doctor', 'frontdesk' ])->default('frontDesk');
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
