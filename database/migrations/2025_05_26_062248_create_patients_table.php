e1a71f6378b35193cab0455fc0c9472755b70587
suzt-0 <suztbhttr2@gmail.com>
2025-07-28 17:03:38 +0545

    remove user_id for unauth patient creation

--- database/migrations/2025_05_26_062248_create_patients_table.php
+++ database/migrations/2025_05_26_062248_create_patients_table.php
@@ -18,8 +18,7 @@ return new class extends Migration {
             $table->date('birth_date')->nullable();
             $table->boolean('active')->default(true);
             $table->timestamps();
-
-            $table->foreignId('user_id')->constrained('users')->nullable()->nullOnDelete();
+            // $table->foreignId('user_id')->constrained('users')->nullable();
         });
     }
 
