140d5448544d6d251545c5be854e2885961f7d5b
suzt-0 <suztbhttr2@gmail.com>
2025-07-28 17:10:21 +0545

    new middlewares to check roles

---
+++ app/Http/Middleware/frontDeskCheck.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class AdminCheck
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        //check if authenticated
+        if (!Auth::check()) {
+            return redirect('/login')->with('error', 'Please log in to access this page.');
+        }
+
+        //check if user is an admin
+        if (Auth::user()->role === 'frontDesk') {
+            return $next($request);
+        } else {
+            return abort(403, 'Unauthorized action.');
+        }
+    }
+}
